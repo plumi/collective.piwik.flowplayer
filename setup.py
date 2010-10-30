5c165fc322050e9dc7051b25c8f8e2dc95170e6c
Dimitris Moraitis <dimo@unweb.me>
2010-10-30 18:20:16 +0000

    update setup.py, add contributors.txt
    svn path=/collective.piwik.flowplayer/trunk/; revision=225671

--- setup.py
+++ setup.py
@@ -1,22 +1,38 @@
 from setuptools import setup, find_packages
 import os
 
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
 version = '0.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Contributors\n'
+    '~~~~~~~~~~~~\n'
+    + '\n' +
+    read('docs/CONTRIBUTORS.txt')
+    + '\n' +
+    read('docs/HISTORY.txt')
+    + '\n' +
+   'Download\n'
+   '--------\n'
+    )
 
 setup(name='collective.piwik.flowplayer',
       version=version,
       description="Analytics support for flowplayer using piwik",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      keywords='video analytics flowplayer piwik',
+      author='unweb.me',
+      author_email='we@unweb.me',
+      url='http://svn.plone.org/svn/collective/collective.piwik.flowplayer',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.piwik'],
