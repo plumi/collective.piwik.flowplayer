c5c9aa133483b8351fc1534ba1aa22aa6c9b5d14
Dimitris Moraitis <dimo@unweb.me>
2010-10-30 17:24:48 +0000

    initial import
    svn path=/collective.piwik.flowplayer/trunk/; revision=225639

---
+++ collective/piwik/flowplayer/viewlets.py
@@ -0,0 +1,6 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets.common import ViewletBase
+
+class TranscodeViewlet(ViewletBase):
+    render = ViewPageTemplateFile('viewlet.pt')
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='collective.piwik.flowplayer',
+      version=version,
+      description="Analytics support for flowplayer using piwik",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.piwik'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'collective.transcode.star',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
