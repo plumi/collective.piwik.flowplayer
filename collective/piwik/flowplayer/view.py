5943352f4d07d7370c462a72d86798867f967257
Dimitris Moraitis <dimo@unweb.me>
2011-01-24 17:33:11 +0000

    invoke piwik api server side for increased security, use collective.piwik.core
    svn path=/collective.piwik.flowplayer/trunk/; revision=231010

---
+++ collective/piwik/flowplayer/view.py
@@ -0,0 +1,55 @@
+from Products.Five.browser  import BrowserView
+import urllib2, simplejson
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets.common import ViewletBase
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+from collective.piwik.core.interfaces import IPiwikSettings
+
+class PlayCountView(BrowserView):
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        self.request.response.setHeader('Content-Type', 'application/json')
+        result = self.getPlayCount()
+        return simplejson.dumps(result)
+
+    def getPlayCount(self):
+        """ Return the number of video play count and downloads from Piwik 
+        """
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IPiwikSettings)
+
+        self.plays = 0
+        self.downloads = 0
+        
+        #for urls with slash(es) on the end
+        self.page_url = self.context.absolute_url().strip('/')        
+        self.page_id = self.page_url.split('/')[-1]
+
+        url = settings.piwik_server +'?module=API&method=Actions.getOutlinks&idSite=' + settings.piwik_siteid + '&period=year&date=last100&format=json&filter_column_recursive=label&filter_pattern_recursive=' +  self.page_id + '&expanded=1&token_auth=' + settings.piwik_key
+
+        try: 
+            piwik_data = simplejson.load(urllib2.urlopen(url))
+        except Exception, e: # might be a URLError, timeout etc
+            piwik_data = {}
+
+        if piwik_data.get('result') == 'error':
+            piwik_data = {} # error on the communication.maybe wrong token?
+
+        for year in piwik_data:
+            if piwik_data[year]:
+                self.check_url(piwik_data[year])
+
+        return(self.plays, self.downloads)
+
+    def check_url(self, entry):
+        for level in entry:
+            if level.get('url'):            
+                if level['url'] == self.page_url:
+                    self.plays += int(level['nb_hits'])
+                    self.downloads += int(level['nb_visits'])
+            elif level.get('subtable'):
+                self.check_url(level['subtable'])
